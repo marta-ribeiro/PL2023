81aa853afa2a086f3502675a73bc774445a25fe2
marta-ribeiro <a95408@alunos.uminho.pt>
2023-02-20 19:15:58 +0000

    tabelas de distribuicao e menu criados

--- TPC1/tpc1.py
+++ TPC1/tpc1.py
@@ -42,8 +42,17 @@ def doenca_por_idade(idade, tem_doenca):
 
 	ordenado = dict(sorted(faixa_etaria.items()))
 	print("Distribuição da doença pela faixa etária: ")
+	print(">----------------|--------------<")
+	print(">-[Faixa etária]-|-[Nº Doentes]-<")
+	print(">----------------|--------------<")
 	for key, valor in ordenado.items():
-		print(f"[{key},{key+4}] - {valor}")
+		if valor <10:
+			print(f">    [{key},{key+4}]     |      {valor}       <")
+		elif valor <100:
+			print(f">    [{key},{key + 4}]     |      {valor}      <")
+		else:
+			print(f">    [{key},{key + 4}]     |      {valor}     <")
+	print(">----------------|--------------<")
 
 
 def doenca_por_colesterol(colesterol, tem_doenca):
@@ -51,6 +60,8 @@ def doenca_por_colesterol(colesterol, tem_doenca):
 
 	for i in range(len(tem_doenca)):
 		if tem_doenca[i] == 1:
+			if colesterol[i] == 0:
+				continue
 			escolha = (colesterol[i] // 10) * 10
 			if escolha in nivel_colesterol:
 				nivel_colesterol[escolha] += 1
@@ -59,8 +70,16 @@ def doenca_por_colesterol(colesterol, tem_doenca):
 
 	ordenado = dict(sorted(nivel_colesterol.items()))
 	print("Distribuição da doença pelos níveis de colesterol: ")
+	print(">---------------------|--------------<")
+	print(">-[Níveis Colesterol]-|-[Nº Doentes]-<")
+	print(">---------------------|--------------<")
 	for key, valor in ordenado.items():
-		print(f"[{key},{key+9}] - {valor}")
+		if valor < 10:
+			print(f">      [{key},{key+9}]      |      {valor}       <")
+		elif valor < 100:
+			print(f">      [{key},{key+9}]      |      {valor}      <")
+		else:
+			print(f">      [{key},{key+9}]      |      {valor}     <")
 
 
 def main():
@@ -72,10 +91,26 @@ def main():
 	batimento = []
 	tem_doenca = []
 	ler_ficheiro(idade, sexo, tensao, colesterol, batimento, tem_doenca)
-	doenca_sexo(sexo, tem_doenca)
-	doenca_por_idade(idade, tem_doenca)
-	doenca_por_colesterol(colesterol, tem_doenca)
 
+	print("O que pretende calcular? ")
+	print("1. Distribuição da doença por sexo.\n"
+		  "2. Distribuição da doença por escalões etários.\n"
+		  "3. Distribuição da doença por níveis de colesterol.\n"
+		  "0. Sair.")
+	opcao = int(input("Escolha: "))
+
+	while opcao != 0:
+		if opcao == 1:
+			doenca_sexo(sexo, tem_doenca)
+		elif opcao == 2:
+			doenca_por_idade(idade, tem_doenca)
+		elif opcao == 3:
+			doenca_por_colesterol(colesterol, tem_doenca)
+		else:
+			print("Valor não é aceite. Tente outra vez.")
+		opcao = int(input("Escolha: "))
+
+	print("Obrigado!")
 
 if __name__ == "__main__":
 	main()
